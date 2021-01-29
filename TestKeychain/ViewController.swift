7985325899ca7b33b5530b5ee2fde9f68d20a9fa
ast <shibzuhov2000@mail.ru>
2021-01-29 06:04:20 +0300

    asd

--- TestKeychain/ViewController.swift
+++ TestKeychain/ViewController.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import KeychainAccess
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let keychain = Keychain(service: "product.astema")
+        if let value = try? keychain.get("Astema") {
+            print("GET DATA IN KEYCHAN \n-----------")
+            print("In keychain found value -- \(value) -- by key # \("Astema") #")
+        } else {
+            print("SAVING DATA IN KEYCHAN \n-----------")
+            keychain["Astema"] = "KEYKEY"
+        }
     }
 
 
